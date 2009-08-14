0b63150df14f622e45a54956d462f9c7c45b901a
Matteo Collina <matteo.collina@gmail.com>
2009-08-14 23:20:09 +0200

    Updated Navigable to add a #check method to controllers.
    Added a check method to all controllers that add a #variable? method
    that checks the presence of an instance variable. Added also a
    check_model method that does the same plus checks if that variable is a
    new record and if so return false.

--- lib/navigations/navigable.rb
+++ lib/navigations/navigable.rb
@@ -5,6 +5,27 @@ module Navigations
       super
       klass.class_inheritable_reader :navigator
       klass.write_inheritable_attribute(:navigator, Navigator.new)
+      klass.send(:extend, ClassMethods)
+    end
+
+    module ClassMethods
+      def check name, code=nil
+        name = name.to_s.underscore
+        code ||= "true"
+        
+        class_eval <<-EOF
+          def #{name}?
+            var = instance_variable_get(:@#{name})
+            return false unless var
+            #{code}
+          end
+EOF
+        self
+      end
+
+      def check_model name
+        check name, "not var.nil? and not var.new_record?"
+      end
     end
   end
 end
--- spec/navigations/navigable_spec.rb
+++ spec/navigations/navigable_spec.rb
@@ -29,5 +29,74 @@ describe Navigable do
     obj.should respond_to(:navigator)
     obj.navigator.should == @klass.navigator
   end
+
+  it "should add a check class method" do
+    @klass.should respond_to(:check)
+  end
+
+  it "should add a check class method to its subclasses" do
+    subclass = Class.new(@klass)
+    subclass.should respond_to(:check)
+  end
+
+  it "should add a check_model class method" do
+    @klass.should respond_to(:check_model)
+  end
+
+  it "should add a check_model class method to its subclasses" do
+    subclass = Class.new(@klass)
+    subclass.should respond_to(:check_model)
+  end
+
+  describe "check class method" do
+    it "should accept a name and creates a name? instance method" do
+      @klass.check :long_name
+      instance = @klass.new
+      instance.should respond_to(:long_name?)
+    end
+
+    it "should creates a name? instance method that checks the presence
+        of an instance variable with the specified name" do
+      @klass.check :object
+      @klass.send(:attr_accessor, :object)
+      instance = @klass.new
+      instance.should_not be_object
+      instance.object = "hello"
+      instance.should be_object
+    end
+
+    it "should creates a name? instance method that execute custum code if the
+       variable is specified" do
+      @klass.check :object, "a_method"
+      @klass.send(:attr_accessor, :object)
+      instance = @klass.new
+      instance.should_not be_object
+      instance.object = "hello"
+      instance.should_receive(:a_method).and_return(true)
+      instance.should be_object
+    end
+  end
+
+  describe "check_model class method" do
+    it "should accept a name and creates a name? instance method" do
+      @klass.check :long_name
+      instance = @klass.new
+      instance.should respond_to(:long_name?)
+    end
+
+    it "should creates a name? instance method that checks the presence " +
+        "of an instance variable with the specified name and is not a new record" do
+      @klass.check_model :object
+      @klass.send(:attr_accessor, :object)
+      instance = @klass.new
+      instance.should_not be_object
+      model = mock "Object"
+      instance.object = model
+      model.should_receive(:new_record?).and_return(true)
+      instance.should_not be_object
+      model.should_receive(:new_record?).and_return(false)
+      instance.should be_object
+    end
+  end
 end
 
